6a93605ee2ee7ec9878782bf9fd9c459aeb00ee9
tosh7 <zlia.6.lj.425@gmail.com>
2024-04-01 20:54:02 +0900

    create CounterView

---
+++ WhitePanda Watch App/Scenes/Counter/CounterView.swift
@@ -0,0 +1,18 @@
+//
+//  CounterView.swift
+//  WhitePanda Watch App
+//
+//  Created by satoshi on 2024/04/01.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        Text("Now Count")
+    }
+}
+
+#Preview {
+    CounterView()
+}
