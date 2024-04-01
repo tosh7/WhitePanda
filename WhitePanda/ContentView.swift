8b73cf3450d9431d1cb43ead014312556dd90870
tosh7 <zlia.6.lj.425@gmail.com>
2024-04-01 19:52:49 +0900

    Initial Commit

---
+++ WhitePanda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhitePanda
+//
+//  Created by satoshi on 2024/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhitePanda/WhitePandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhitePandaApp.swift
+//  WhitePanda
+//
+//  Created by satoshi on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct WhitePandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
