be1d60795b0910526c172cf11c51316bf3ac42c4
tosh7 <zlia.6.lj.425@gmail.com>
2024-04-13 01:52:18 +0900

    create RoundFeature

---
+++ WhitePanda Watch App/Scenes/Round/RoundReducer.swift
@@ -0,0 +1,61 @@
+//
+//  RoundReducer.swift
+//  WhitePanda Watch App
+//
+//  Created by satoshi on 2024/04/13.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct RoundFeature {
+
+    @ObservableState
+    struct State: Equatable {
+        var count: Int = 0
+        var roundCount: Int = 1
+        var type: RoundType
+    }
+
+    enum Action {
+        // ViewEvents
+        case decrementButtonTapped
+        case incrementButtonTapped
+        case resetButtonTapped
+        case goNextButtonTapped
+        case goPreviousButtonTapped
+        // transitions
+        case resultScene
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                if state.count > 0 {
+                    state.count -= 1
+                }
+                return .none
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            case .resetButtonTapped:
+                state.count = 0
+                return .none
+            case .goNextButtonTapped:
+                return .none
+            case .goPreviousButtonTapped:
+                return .none
+            case .resultScene:
+                return .none
+            }
+        }
+    }
+}
+
+
+enum RoundType: Int {
+    case half = 9
+    case full = 18
+}
---
+++ WhitePanda Watch App/Scenes/Round/RoundView.swift
@@ -0,0 +1,18 @@
+//
+//  RoundView.swift
+//  WhitePanda Watch App
+//
+//  Created by satoshi on 2024/04/13.
+//
+
+import SwiftUI
+
+struct RoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundView()
+}
