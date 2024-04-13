a8bca4259d16c32f8c48d4be33e21253a9a8f8dc
tosh7 <zlia.6.lj.425@gmail.com>
2024-04-13 12:30:21 +0900

    create RoundModel

---
+++ CommonModel/Sources/CommonModel/RoundModel.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Satoshi Komatsu on 2024/04/13.
+//
+
+import Foundation
+
+public final class RoundModel {
+    var type: RoundType
+    var counts: [CountModel] = []
+    var totalScore: Int {
+        return counts.reduce(0) {
+            $0 + $1.count
+        }
+    }
+
+    public init(type: RoundType) {
+        self.type = type
+    }
+}
+
+// FIXME: This cound be replaced with Int, but use struct just for further updates
+public struct CountModel: Equatable, Identifiable {
+    public var id: UUID = UUID()
+    var count: Int
+//    var per: Int
+
+    public init(count: Int) {
+        self.count = count
+    }
+}
